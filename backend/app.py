04212f1173465a6f7c71681aea7bdfaba6c8ca70
Tommaso Sorrentino <83490128+LobsterRavioli@users.noreply.github.com>
2025-06-21 13:12:21 +0200

    changed the log

--- backend/app.py
+++ backend/app.py
@@ -33,7 +33,7 @@ init_counter()
 def count():
     item = container.read_item(item="main", partition_key="main")
     item["count"] += 1
-    print(f"Visite aggiornate: {item['count']}")  # QUESTO verrà mostrato nei log
+    print(f"Visite aggiornate: {item['count']}", file=sys.stdout, flush=True)
     container.replace_item(item="main", body=item)
 
     return jsonify(count=item["count"]), 200
