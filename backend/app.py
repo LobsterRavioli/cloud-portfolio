fecd3dab2251c0df38674138c3bd7924ef6433c2
Tommaso Sorrentino <83490128+LobsterRavioli@users.noreply.github.com>
2025-06-24 12:58:18 +0200

    Importing Response module from flask in the app

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 from flask_cors import CORS
 from azure.cosmos import CosmosClient, PartitionKey, exceptions
 # Import prometheus client
